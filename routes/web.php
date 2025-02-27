7c0e7d0b07bf84d777cb05096f10fabbb058c7fc
Junior Martinez <junior15062023@gmail.com>
2025-02-27 10:38:04 -0500

    router in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\LaboratoryController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use App\Http\Controllers\CategoryController;
-
-
-
+use App\Http\Controllers\SupplierController;
 
 // SYSTEM START
 Route::get('/', [DashboardController::class, 'Welcome'])->name('Welcome');
@@ -20,14 +16,16 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',
     Route::prefix('modulo')->group(function () {
         // CRUD MODULES
         Route::resource('doctor', DoctorController::class)->except(['create']);
-        Route::resource('category', LaboratoryController::class)->except(['create']);
+        Route::resource('category', CategoryController::class)->except(['create']);
         Route::resource('laboratories', LaboratoryController::class)->except(['create']);
+        Route::resource('supplier', SupplierController::class)->except(['create']);
         // path/route to list
         Route::get('doctors/list', [DoctorController::class, 'listDoctor'])->name('doctor.list');
         Route::get('laboratories/list', [LaboratoryController::class, 'listLaboratory'])->name('laboratory.list');
         Route::get('categories/list', [CategoryController::class, 'listCategory'])->name('category.list');
     });
 });
+
+
 Route::get('doctors/search', [DoctorController::class, 'searchDoctor'])->name('doctor.search');
 Route::get('laboratories/search', [LaboratoryController::class, 'searchLaboratory'])->name('laboratory.search');
-
